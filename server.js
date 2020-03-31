2ee8d26f535f7839b6d856e48c05810e46d33a87
Inrikys <henrique661@gmail.com>
2020-03-30 22:28:39 -0300

    Alteração no caminho da pasta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const app = require('../Urna/bin/express');
-const variables = require ('../Urna/bin/configuration/variables');
+const app = require('./bin/express');
+const variables = require ('./bin/configuration/variables');
 
 app.listen(variables.Api.port, () => {
     console.log('Servidor Urna Api iniciado na porta ' + variables.Api.port);
